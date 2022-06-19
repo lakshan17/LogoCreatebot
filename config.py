3138a95a47f024f9e605118955b0173434283375
Lakshan Pathum <107621003+lakshanpathum17@users.noreply.github.com>
2022-06-19 16:28:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from import os
+
+BOT_TOKEN = environ.get("BOT_TOKEN", "5367701836:AAEitY80_xke2K3KyhPnpsGP0RKJtO3GP5E")
+
+API_ID = int(environ.get("API_ID",10432339))
+
+API_HASH = environ.get("API_HASH", "a6d7561a8ed9f505a47672c6bd8a2426")
